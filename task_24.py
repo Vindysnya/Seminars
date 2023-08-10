0326554b6328a43efab6c9397d2a1b1ee53a12ba
Илья Борисов <ilyaborisov086@gmail.com>
2023-08-10 09:55:24 +0300

    task_24

--- task_24.py
+++ task_24.py
@@ -1,1 +1,4 @@
-N = int(input("Введите кол-во кустов черники: "))
+lst = [1, 2, 58, 4, 5, 6, 7, 8]
+res = max([(lst[i - 1] + lst[i] + lst[(i + 1) % len(lst)], i + 1)
+          for i in range(len(lst))])
+print(f' Макс. кол-во ягод {res[0]}, собрано для куста {res[1]}')
